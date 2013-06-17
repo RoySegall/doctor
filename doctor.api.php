1207121d41c072bf5ff9a9b8ffa9369f60de08d5
RoySegall <roy.segall@gizra.com>
2013-06-17 21:37:40 +0300

    Start working on the UI display handler.

--- doctor.api.php
+++ doctor.api.php
@@ -39,6 +39,46 @@ function hook_doctor_listener_info_alter(&$data) {
 }
 
 /**
+ * Implements hook_listener_ui_info().
+ *
+ * Unlike the normal listen item, this hook will define which hook will be
+ * implements when watching the reports in the admin page.
+ */
+function hook_listener_ui_info() {
+  return array(
+    'doctor' => array(
+      'callback' => '_hook_doctor_listener_ui_info',
+    ),
+  );
+}
+
+/**
+ * Doctor listener callback function of the UI.
+ *
+ * @param $data
+ *  The un-serialized data listener item.
+ *
+ * @return string
+ *  The string to output in the reports page.
+ */
+function _hook_doctor_listener_ui_info($data) {
+  return t('Your recorded info: @data', array('@data' => $data));
+}
+
+/**
+ * Implements hook_listener_ui_info_alter().
+ *
+ * Allow you to alter the listener callback functions.
+ */
+function hook_listener_ui_info_alter(&$data) {
+  foreach ($data as $module_name => &$callbacks_info) {
+    if ($module_name == 'doctor') {
+      $callbacks_info['callback'] = '__hook_doctor_listener_info';
+    }
+  }
+}
+
+/**
  * Implements hook_doctor_bundle_info().
  */
 function hook_doctor_bundle_info() {
