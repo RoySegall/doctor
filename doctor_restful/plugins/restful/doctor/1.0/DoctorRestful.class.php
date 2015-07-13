cbc73d99dccdc37e3b391561f2bf5827a9429d0f
Roy Segall <roy.segall@gizra.com>
2015-07-13 10:00:13 +0300

    Adding doctor restful

---
+++ doctor_restful/plugins/restful/doctor/1.0/DoctorRestful.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains RestfulExampleArticlesResource.
+ */
+
+class DoctorRestful extends RestfulEntityBase {
+
+  public function publicFieldsInfo() {
+    $info = parent::publicFieldsInfo();
+
+    
+    return $info;
+  }
+
+}
