665b51a404dbcfabc5bfd28c4d705c519e183fe4
Roy Segall <roy.segall@gizra.com>
2015-07-13 10:11:13 +0300

    Add more fields.

--- doctor_restful/plugins/restful/doctor/1.0/DoctorRestful.class.php
+++ doctor_restful/plugins/restful/doctor/1.0/DoctorRestful.class.php
@@ -10,7 +10,22 @@ class DoctorRestful extends RestfulEntityBase {
   public function publicFieldsInfo() {
     $info = parent::publicFieldsInfo();
 
-    
+    $info['created'] = array(
+      'property' => 'created',
+    );
+
+    $info['data'] = array(
+      'property' => 'data',
+    );
+
+    $info['watched'] = array(
+      'property' => 'watched',
+    );
+
+    $info['type'] = array(
+      'property' => 'type',
+    );
+
     return $info;
   }
 
